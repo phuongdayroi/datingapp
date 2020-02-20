18d9b3121b740ceedc0c735126da752f5f2fff4a
Jeremy <jeremy@enclave.vn>
2020-02-20 17:27:39 +0700

    add user model service in client

---
+++ DatingApp.Client/DatingAppSPA/src/app/_services/user.service.ts
@@ -0,0 +1,26 @@
+import { Observable } from "rxjs";
+import { environment } from "./../../environments/environment";
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { User } from "../models/user";
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: "Bearer " + localStorage.getItem("token")
+  })
+};
+
+@Injectable({
+  providedIn: "root"
+})
+export class UserService {
+  baseUrl = environment.apiUrl;
+  constructor(private http: HttpClient) {}
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl + "users", httpOptions);
+  }
+
+  getUser(id): Observable<User> {
+    return this.http.get<User>(this.baseUrl + "users/" + id, httpOptions);
+  }
+}
